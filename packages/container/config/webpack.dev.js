48c4184939788a2c1506085dd05d4b0310f448de
Abrham Getachew <abrhamcisco@gmail.com>
2022-03-21 13:17:33 +0300

    Initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const {merge} = require('webpack-merge')
+//merge is used to merge 2 or more webpack configs.
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const packageJson = require('../package.json')
+
+const devConfig = {
+    mode:'development',
+    devServer:{
+        port:8080,
+        historyApiFallback:{
+            index:'index.html'
+        }
+    },
+    plugins:[
+        new ModuleFederationPlugin({
+            name:'container',
+            remotes:{
+                'marketing':'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared:packageJson.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig,devConfig)
+//beacuse devConfig is the right most it will overwrite if there are conflicts.
---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,21 @@
+// @babel/preset-react : for converting jsx to js
+// @babel/preset-env : for converting ES 2015, 17, 18, ... to ES5
+// @babel/plugin-transform-runtime : adds async await and another features.
+
+module.exports = {
+    module:{
+        rules:[
+            {
+                test:/\.m?js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-react','@babel/preset-env'],
+                        plugins:['@babel/plugin-transform-runtime']
+                    }
+                }
+            }
+        ]
+    }
+}
