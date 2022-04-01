eb9fae5d7d74a5edaf5f0465bd0e5db0eab29edf
Abrham Getachew <abrhamcisco@gmail.com>
2022-04-01 15:02:06 +0300

    Marketing and Container routes are in sync and basic auth components have been created

---
+++ packages/auth/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const {merge} = require('webpack-merge')
+//merge is used to merge 2 or more webpack configs.
+const commonConfig = require('./webpack.common')
+const ModuleFedrationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const packageJson = require('../package.json')
+const devConfig = {
+    mode:'development',
+    devServer:{
+        port:8082,
+        historyApiFallback:{
+            index:'index.html'
+        }
+    },
+    plugins:[
+        new ModuleFedrationPlugin({
+            name:'auth',
+            filename:'remoteEntry.js',
+            exposes:{
+                './AuthApp':'./src/bootstrap'
+            },
+            shared:packageJson.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig,devConfig)
+//beacuse devConfig is the right most it will overwrite if there are conflicts.
